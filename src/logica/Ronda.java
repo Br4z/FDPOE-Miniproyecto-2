91b0626d688670f1377bbf8ba9d14eec759c06e9
Sebastián <sebastianidrobo@hotmail.com>
2022-06-20 15:20:04 -0500

    Clase Ronda añadida - La clase "Ronda" fue añadida. - Se encuentra almacenada en el paquete "logica". - Dentro de ella se encuentran los atributos, pero no sus métodos. - Los atributos se hicieron en base al lucidchart.

---
+++ src/logica/Ronda.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author Brandon Calderón <calderon.brandon@correounivalle.edu.co>
+ * @author Sebastian Idrovo Avirama <idrobo.sebastian@correounivalle.edu.co>
+ * @profesor Luis Yovany Romo Portilla
+ * Clase que representa la ronda de juego respectiva
+ */
+
+public class Ronda {
+    private int vidas;
+    private int puntaje;
+    private int aciertos;
+    private int velocidadActual;
+    private int cantidadBaldosas;
+    
+}
